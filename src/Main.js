9c489521217a02c26745cd0e84068e8efd77bf14
Jackson Gurney <jacko7557@gmail.com>
2022-08-16 16:11:35 -0700

    Reached target goals for today.

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,25 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import data from "./data.json"
+import "./Main.css";
 
 class Main extends React.Component {
   render() {
+    let animals = [];
+    data.forEach(element => {
+      animals.push(
+        <HornedBeast
+          imageUrl={element.image_url}
+          description={element.description}
+          alt={element.keyword}
+          horns={element.horns}
+          title={element.title}
+        />
+      )
+    });
     return (
       <main>
-        <HornedBeast
-          title="Narwhale"
-          imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/%D0%9D%D0%B0%D1%80%D0%B2%D0%B0%D0%BB_%D0%B2_%D1%80%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B9_%D0%90%D1%80%D0%BA%D1%82%D0%B8%D0%BA%D0%B5.jpg/1200px-%D0%9D%D0%B0%D1%80%D0%B2%D0%B0%D0%BB_%D0%B2_%D1%80%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B9_%D0%90%D1%80%D0%BA%D1%82%D0%B8%D0%BA%D0%B5.jpg"
-          description="Its a sea unicorn"
-        />
-        <HornedBeast
-          title="Male Impala"
-          imageUrl="https://upload.wikimedia.org/wikipedia/commons/5/53/Male_impala_profile.jpg"
-          description="He is eating small leafs"
-        />
+        {animals}
       </main>
     );
   }
