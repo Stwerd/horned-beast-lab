3a9d85ca5fa7daf8fac32f4bb816cd7952fe718f
Jackson Gurney <jacko7557@gmail.com>
2022-08-18 16:43:01 -0700

    reached target

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,55 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import "./Main.css";
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 1,
+      filteredData: this.props.data
+    }
+  }
+  handleChange = (event) => {
+    event.preventDefault();
+    let selected = event.target.value;
+    this.setState({
+      value: selected
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let newData = this.props.data.filter(elm => elm.horns == this.state.value)
+    this.setState({ filteredData: newData });
+  }
+
+
   render() {
-    let animals = this.props.data.map(element => {
-        return <HornedBeast
-          temp={this.props.temp}
-          animal={element}
-        />
+    let animals = this.state.filteredData.map(element => {
+      return <HornedBeast
+        temp={this.props.temp}
+        animal={element}
+      />
     });
     return (
       <main>
+        <Form onSubmit={this.handleSubmit}>
+          <legend>Filter by Horns
+            <Form.Group>
+              <Form.Label>Selected Numbers</Form.Label>
+              <Form.Select style={{width:"20%"}} className="options" name="selected" onChange={this.handleChange}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="100">100</option>
+              </Form.Select>
+            </Form.Group>
+            <button type="submit">Submit</button>
+          </legend>
+        </Form>
         {animals}
       </main>
     );
