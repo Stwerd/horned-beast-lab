c29e37a74a2924dd6b35272e2ac67805ca805d2f
Jackson Gurney <jacko7557@gmail.com>
2022-08-17 22:36:45 -0700

    Tried to get it to work, need to watch the lab.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import data from "./data.json"
 
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Header />
+        <Main 
+        data={data}
+        />
+        <Footer />
       </>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,17 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import data from "./data.json"
 import "./Main.css";
 
 class Main extends React.Component {
   render() {
-    let animals = [];
-    data.forEach(element => {
-      animals.push(
-        <HornedBeast
+    let animals = this.props.data.map(element => {
+        return <HornedBeast
           imageUrl={element.image_url}
           description={element.description}
           alt={element.keyword}
           horns={element.horns}
           title={element.title}
         />
-      )
     });
     return (
       <main>
